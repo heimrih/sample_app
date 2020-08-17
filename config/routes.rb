92b50f6411936c379edb4e9ad29cab00185209da
heimrih <heimrih214@gmail.com>
2020-08-17 18:19:48 +0800

    Chapter8
    Fix Chapter8
    
    Fix2 Chaper8
    
    Fix3 Chaper8
    
    Fix4 Chapter8

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     get "/about", to: "static_pages#about", as: :about
     get "/help", to: "static_pages#help", as: :help
     root "static_pages#home"
+    get "/login", to: "sessions#new", as: :login
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy", as: :logout
     resources :users
   end
 end
